2fb061a21cd855d7744714d92d1f62459b2aa1b3
Mykhailo Solop <mixasolop@gmail.com>
2025-02-22 00:46:11 +0300

    made working function min

--- file_csv.py
+++ file_csv.py
@@ -3,7 +3,7 @@ import csv
 import scrap
 
 filepath = 'scrap.csv'
-data = pd.read_csv(filepath, usecols=["bid"])
+data = pd.read_csv(filepath)
 print("enter what do you want to do: \nscan - scrap current exchanging rates\nmin - find minimum value at the specific currency")
 command = input()
 
@@ -11,3 +11,15 @@ command = input()
 
 if(command == "scan"):
     scrap.scan_check()
+    
+elif(command == "min"):
+    print("please provide what currency minimum you want to know?")
+    currency = input()
+    values = data.loc[data["from/to"] == currency, "bid"]
+    min = values.values[0]
+    min_idx = 0
+    for i in range(len(values.values)):
+        if(values.values[i] <= min):
+            min = values.values[i]
+            min_idx = values.index[i]
+    print(f"the least value of {currency} was {min} at {data.loc[min_idx, 'time']}")
